cd45a9456c70faa03c216de99f8b436d4dc958aa
Sunil kumar <67591692+sunil-dangi-au13@users.noreply.github.com>
2022-06-14 13:03:51 +0530

    Add files via upload
    Dsa task

---
+++ pattern.js
@@ -0,0 +1,12 @@
+var i;
+var j;
+var k =1;
+var z = 1;
+var spaces;
+for(i=0;i<=5;i++){
+    for(j=1;j<=i;j++){
+        document.write(k+"&nbsp;");
+        k=k+2;
+    }
+    document.write("<break>");
+}
